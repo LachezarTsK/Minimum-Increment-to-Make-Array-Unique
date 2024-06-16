3bcaf1f89206f7e39857deb8c213df8246258bd2
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-16 17:01:29 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,34 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+
+    static const int MIN_LIMIT = 0;
+
+public:
+    int minIncrementForUnique(const vector<int>& input) const {
+        int maxValue = *ranges::max_element(input);
+        vector<int> frequency(maxValue + 1);
+        for (const auto& value : input) {
+            ++frequency[value];
+        }
+
+        int minIncrementCount = 0;
+        for (int value = MIN_LIMIT; value < maxValue; ++value) {
+            if (frequency[value] > 1) {
+                minIncrementCount += frequency[value] - 1;
+                frequency[value + 1] += frequency[value] - 1;
+            }
+        }
+
+        return (frequency[maxValue] > 1)
+                ? (minIncrementCount + sumAllValuesFromOneToN(frequency[maxValue] - 1))
+                : minIncrementCount;
+    }
+
+private:
+    int sumAllValuesFromOneToN(int n)const {
+        return n * (n + 1) / 2;
+    }
+};
